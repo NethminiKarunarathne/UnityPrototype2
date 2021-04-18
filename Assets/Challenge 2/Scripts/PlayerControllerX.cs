0e8c6fac2c0d7a404ea2a48f05b91b186a27d34d
Nethmini Karunarathne <nethminikarunarathne@gmail.com>
2021-04-18 09:12:56 +0530

    Add Unity Project2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    
+    public float clonerate = 0.1f;
+    public float nextClone = 0;
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > nextClone)
+        {
+            nextClone = Time.time + clonerate;
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Debug.Log("next sapce bar available in :" + nextClone );
+            
+        }
+        
+    }
+}
